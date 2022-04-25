f64d3bc7f18f49f009e947dbfead6b3513b2a803
Michael Kamau <kamau.karitu@gmail.com>
2022-04-25 12:10:53 +0300

    Contact page design

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,12 +1,14 @@
 import { createApp } from "vue";
 
 import HelloWorld from "./components/HelloWorld";
-import Navbar from "./components/Navbar";
+import Navbar from "./components/Navigation/Navbar";
+import ContactForm from "./components/Myapp/Contact/ContactForm"
 
 const app = createApp({});
 
 app.component("hello-world", HelloWorld);
 app.component("navbar", Navbar);
+app.component("contact-form", ContactForm);
 
 
 
