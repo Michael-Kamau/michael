cfb84852e850347b4e11ff3e6d209c350705ec30
Michael Kamau <kamau.karitu@gmail.com>
2022-05-12 14:19:52 +0300

    Send emails to admin from the contact form

--- src/myapp/views.py
+++ src/myapp/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-# Create your views here.
+from django.core.mail import send_mail
+import json
 
 
 def about(request):
@@ -12,6 +13,18 @@ def applications(request):
 	return render(request,'myapp/applications.html')
 
 def contacts(request):
-	return render(request,'myapp/contacts.html')
+	if request.method == 'POST':
+		body_unicode = request.body.decode('utf-8')
+		body = json.loads(body_unicode)
+ 
+		name = body['name']
+		email = body['email']
+		message = body['message']
+
+		send_mail(name,message,email,['testmail@gmail.com'], fail_silently = False)
+		return render(request,'myapp/contacts.html')
+	else:
+		return render(request,'myapp/contacts.html')
+
 def home(request):
 	return render(request,'home.html')
